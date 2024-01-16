2457ab9d4aae705b73c363218f101c3a3189ee5d
Mark Standen <no-reply@rundog.dev>
2024-01-16 07:29:38 +0000

    added video attributes to autoplay on iOS and loop

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -4,7 +4,7 @@ function Card(props:{cardData: CardData}) {
     return (
         <article className={card.base} title={props.cardData.name}>
             <a href={props.cardData.link}>
-                <video className={card.image} autoPlay={true} muted={true}>
+                <video className={card.image} autoPlay={true} muted={true} loop={true} playsInline={true}>
                     <source src={props.cardData.src} type="video/mp4"/>
                 </video>
             </a>
