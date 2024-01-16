f2ee403bc6080949f23fa072266d27cf1534e605
Mark Standen <no-reply@rundog.dev>
2024-01-16 05:33:55 +0000

    Added react useState and useEffect to manage loading of cards with a button at the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,23 @@ import {Cards} from "./Cards/Cards.tsx";
 import {getData} from "./getData";
 import {convertData} from "./convertData.ts";
 import {apiConfig} from "./apiConfig.ts";
+import {useEffect, useState} from "react";
 
-const pageData = convertData(await getData(apiConfig)(0));
+const getPage = getData(apiConfig);
 
 function App() {
+    const [page, setPage] = useState<number>(0);
+    const [pageData, setPageData] = useState<CardData[]>([]);
+
+    useEffect(() => {
+        async function updateData(page: number): Promise<void> {
+            const newPageData = convertData(await getPage(page));
+            setPageData([...pageData, ...newPageData])
+        }
+
+        updateData(page);
+    }, [page]);
+
     return (<>
         <header>
             <h1>I don't know how to put this</h1>
@@ -14,6 +27,9 @@ function App() {
         <main>
             <Cards data={pageData}/>
         </main>
+        <button onClick={() => {setPage(page+1)}}>
+            GIVE ME MORE
+        </button>
     </>)
 }
 
